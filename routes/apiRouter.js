1150cedcc398ad31eb9c802c66da46b363c1813a
MrRincon <alamrdrgz98@gmail.com>
2024-11-19 09:21:07 +0000

    Adding the use of routers and extra middleware

---
+++ routes/apiRouter.js
@@ -0,0 +1,20 @@
+// Import the necessary modules
+const express = require('express');
+// Defining the array of IP addresses that are allowed to access the API
+const ALLOWED_IPS = ['127.0.0.1', '123.456.7.89'];
+// Creating a new router instance from express to handle API routes and middleware
+const api = express.Router();
+// Extra Middleware to check if the incoming request's IP address is allowed access
+api.use(function(req,res,next) {
+    // Checks if the request's IP is in the list of allowed IPs
+    const userIsAllowed = ALLOWED_IPS.indexOf(req.ip) !== -1;
+    if (!userIsAllowed){
+        // Sends a 401 (Unauthorised) response if the IP is not allowed
+        res.status(401).send('Not authorised!');
+    } else {
+        // Proceeds with the next step after this Middleware
+        next();
+    }
+});
+// Exports all the api functions
+module.exports = api;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');// Main server framework
 const morgan = require('morgan');// Logger middleware to track incoming requests
 const path = require('path');// To work with file and directory paths
+const api = require('./routes/apiRouter.js');
 
 // Define port which the server will listen
 const port = 3000;
@@ -16,6 +17,8 @@ var publicPath = path.join(__dirname, '../Site');// Locate the 'Site' folder
 var imagePath = path.join(__dirname, '../Site/images');
 app.use(express.static(publicPath));// Using the path with express.static so express can server the files from the folder
 app.use(express.static(imagePath));
+
+app.use(api);// Using a router to implement authorisation and manage all the GET and POST 
 
 // Third Middleware to handle 404 errors
 app.use(function(req, res){
