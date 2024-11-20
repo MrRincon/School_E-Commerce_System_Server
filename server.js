9306c929acf4f3c1dc6667c407b311823985f56c
MrRincon <alamrdrgz98@gmail.com>
2024-11-20 16:20:03 +0000

    updating the port

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use(function(req, res){
     res.status(404).send(`${res.statusCode}: File not found!`);
 });
 // Starting the server on the defined port (3000)
-app.listen(port, '0.0.0.0', ()=>{
+app.listen(port, ()=>{
     console.log(`App started on port http://localhost:${port}`);
 });
